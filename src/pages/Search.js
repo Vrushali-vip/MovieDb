2db6164bc7fb34fc9d85448c8d410db7b3411aed
Vrushali <Vrushali.vip2002@gmai.com>
2024-08-08 13:07:49 +0530

    commit

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// src/pages/Search.js
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useLocation } from 'react-router-dom';
+import { fetchMovies } from '../redux/searchSlice';
 import MovieCard from '../components/MovieCard';
 
 const useQuery = () => {
@@ -9,21 +11,21 @@ const useQuery = () => {
 
 const Search = () => {
   const query = useQuery().get('query');
-  const [movies, setMovies] = useState([]);
+  const dispatch = useDispatch();
+  const { movies, loading, error } = useSelector((state) => state.search);
 
   useEffect(() => {
     if (query) {
-      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=YOUR_API_KEY&language=en-US&query=${query}&page=1`)
-        .then(response => {
-          setMovies(response.data.results);
-        });
+      dispatch(fetchMovies(query));
     }
-  }, [query]);
+  }, [query, dispatch]);
 
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-3xl mb-4">Search Results</h1>
-      <div className="grid grid-cols-4 gap-4">
+    <div className="container mx-auto py-8 px-4">
+      <h1 className="text-2xl md:text-3xl font-bold mb-4">Search Results</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error}</p>}
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
         {movies.map(movie => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+// src/redux/store.js
+import { configureStore } from '@reduxjs/toolkit';
+import searchReducer from './searchSlice';
+
+const store = configureStore({
+  reducer: {
+    search: searchReducer,
+  },
+});
+
+export default store;
